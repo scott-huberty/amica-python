d72ef9b280ff704b0a7d7b0fbb2c06ec4ea5d483
Scott Huberty <52462026+scott-huberty@users.noreply.github.com>
2025-09-30 08:22:09 -0700

    Add helpers for writing data and params file for fortran program (#5)
    * Add helpers for writing data and params file for fortran program
    
    * FIX: read

---
+++ src/amica/utils/tests/test_fortran.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from amica.utils.fortran import (
+    load_data,
+    write_param_file,
+    write_data,
+)
+from amica.datasets import data_path
+import mne
+from numpy.testing import assert_allclose
+
+def test_write_param_file(tmp_path):
+
+    fpath = data_path() / "eeglab_sample_data" / "eeglab_data.set"
+    raw = mne.io.read_raw_eeglab(fpath, preload=True)
+    data = raw.get_data().T
+
+    param_fpath, _ = write_param_file(
+        tmp_path / "foo.param",
+        files="./tests/eeglab_sample_data/eeglab_data.fdt",
+        outdir="./tests/eeglab_sample_data/amicaout_test/",
+        data=data,
+        **{
+            "block_size": 512,
+            "blk_min": 256,
+            "blk_step": 256,
+            "blk_max": 1024,
+            "writestep": 20,
+        },
+    )
+    content = param_fpath.read_text()
+    want = (Path(__file__).parent / "assets" / "amicadefs_test.param").read_text()
+    assert content == want
+
+
+def test_io(tmp_path):
+    """Test reading and writing binary data files for Fortran AMICA."""
+    fpath = data_path() / "eeglab_sample_data" / "eeglab_data.set"
+    raw = mne.io.read_raw_eeglab(fpath, preload=True)
+    data = raw.get_data().T
+    fpath = tmp_path / "data.bin"
+    write_data(data, fpath)
+    data_in = load_data(fpath, shape=data.T.shape).T
+    assert_allclose(data.astype("<f4"), data_in)
