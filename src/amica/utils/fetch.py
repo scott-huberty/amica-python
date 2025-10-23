7b07787065763e145c37191e5d6315ce9b293d1f
Scott Huberty <seh33@uw.edu>
2025-10-22 23:59:52 -0700

    FIX: kwarg

--- src/amica/utils/fetch.py
+++ src/amica/utils/fetch.py
@@ -77,7 +77,7 @@ def fetch_fortran_outputs() -> Path:
     """
     import pooch
 
-    unpack = pooch.Untar(extract_dir=".", filter="fully_trusted")
+    unpack = pooch.Untar(extract_dir=".")
     outputs_dir = pooch.retrieve(
         url=FORTRAN_URL,
         known_hash=FORTRAN_HASH,
