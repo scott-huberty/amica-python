484a17852a1204a4cf7e62165e983b474ed74dac
Scott Huberty <seh33@uw.edu>
2025-10-23 22:44:16 -0700

    FIX: sort

--- src/amica/tests/test_check_estimator.py
+++ src/amica/tests/test_check_estimator.py
@@ -1,8 +1,10 @@
 import sys
+
 import pytest
 from sklearn.utils.estimator_checks import parametrize_with_checks
 
 from amica import AMICA
+
 
 @pytest.mark.skipif(sys.platform == "win32", reason="Numerical Failures on Windows")
 @parametrize_with_checks([AMICA()])
