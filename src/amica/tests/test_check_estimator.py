df16ee294bb41507e02bac47d3025859b306c054
Scott Huberty <seh33@uw.edu>
2025-10-26 15:08:00 -0700

    FIX: flakes

--- src/amica/tests/test_check_estimator.py
+++ src/amica/tests/test_check_estimator.py
@@ -1,9 +1,9 @@
-import sys
 
 import pytest
 from sklearn.utils.estimator_checks import parametrize_with_checks
 
 from amica import AMICA
+
 
 @pytest.mark.sklearn_api
 @parametrize_with_checks([AMICA(random_state=0)])
