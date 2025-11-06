30f63c6e1d43def13468521b7f9d48b5a02446fe
Scott Huberty <seh33@uw.edu>
2025-11-05 20:35:14 -0800

    TST: test batch mechanism

--- src/amica/tests/test_amica.py
+++ src/amica/tests/test_amica.py
@@ -515,7 +515,9 @@ def test_sklearn_tutorial_data(do_newton):
 
     X = S @ A.T                               # Observed mixtures
     # Run AMICA
-    modout = fit_amica(X, n_components=3, random_state=0, do_newton=do_newton, batch_size=500)
+    modout = fit_amica(
+        X, n_components=3, random_state=0, do_newton=do_newton, batch_size=499
+        )
     mean_py = modout["mean"] # feature means (3,)
     S_py = modout["S"] # whitening matrix shape (3, 3)
     A_py = modout["A"] # mixing matrix (3, 3)
---
+++ src/amica/tests/test_batching.py
@@ -0,0 +1,30 @@
+import torch
+
+from amica._batching import BatchLoader
+
+
+def test_batch_loader():
+    """Test accessing input data in batches using BatchLoader."""
+    # Vector of 1000 1s, 1000 2s, 1000 3s
+    X = torch.concatenate(
+        [torch.ones((1000, 2)), 2 * torch.ones((1000, 2)), 3 * torch.ones((1000, 2))]
+        )
+    batch_loader = BatchLoader(X, axis=0, batch_size=1000)
+
+    # There should be 3 batches
+    assert len(batch_loader) == 3
+    # Test __getitem__
+    assert torch.all(batch_loader[0] == 1)
+    assert torch.all(batch_loader[1] == 2)
+    assert torch.all(batch_loader[2] == 3)
+    # Test __iter__
+    for i, (batch, batch_slice) in enumerate(batch_loader):
+        expected_value = i + 1
+        assert torch.all(batch == expected_value)
+        assert batch_slice == slice(i * 1000, (i + 1) * 1000)
+    # Test __repr__
+    repr_str = repr(batch_loader)
+    assert "Data shape: torch.Size([3000, 2])" in repr_str
+    assert "Batched axis: 0" in repr_str
+    assert "batch_size: 1000" in repr_str
+    assert "n_batches: 3" in repr_str
