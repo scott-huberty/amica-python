d1350ee0e3c118bc70f47ab12715a9929ab2745f
Scott Huberty <seh33@uw.edu>
2026-05-11 22:01:20 -0700

    version

--- src/amica/__init__.py
+++ src/amica/__init__.py
@@ -1,5 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from . import datasets, utils
 from ._sklearn_interface import AMICA
 from .core import fit_amica
 
-__all__ = ['fit_amica', 'AMICA', 'datasets', 'utils']
+try:
+    __version__ = version("amica-python")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
+
+__all__ = ['fit_amica', 'AMICA', 'datasets', 'utils', '__version__']
