bae5c8dff613c42c8aae677e1804ddab7f31aed4
Scott Huberty <seh33@uw.edu>
2026-07-22 07:42:22 -0700

    DOC: update tut

--- docs/source/examples/plot_eeg.py
+++ docs/source/examples/plot_eeg.py
@@ -70,16 +70,6 @@ transformer = AMICA(
         mu_init=initial_locations,
         tol=1e-7,
         verbose=2,
-        optimizer="daarem",
-        optimizer_kwargs={
-            "accelerator_order": 3,
-            "accelerator_damping": 0.8,
-            "accelerator_ridge": 1e-8,
-            "accelerator_eps_monotone": 0,
-            "accelerator_start_iter": 20,
-            "accelerator_period": 3,
-            "accelerator_validate_candidate": True,
-        },
 )
 transformer.fit(data)
 
